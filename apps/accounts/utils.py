71e120c19dcf9dc74022c7d66faf56a5e4f9d59b
Tomáš Mach <52257177+tomasmach@users.noreply.github.com>
2025-12-24 22:30:43 +0100

    Remove unused import

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -13,8 +13,6 @@ from django.core.mail import send_mail
 from django.core.signing import TimestampSigner, BadSignature, SignatureExpired
 from django.template.loader import render_to_string
 from django.urls import reverse
-from django.utils.html import strip_tags
-import sys
 
 import logging
 
